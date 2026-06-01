81685608c3b5f4915fce9d1bb63e0a7d1a219e01
Darshan Deore <darshan@Darshans-MacBook-Air.local>
2026-06-01 08:51:38 +0530

    chore: update Microsoft Clarity tracking ID to x012rkkk1n

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window,document,"clarity","script","wyzcifskrr");
+            })(window,document,"clarity","script","x012rkkk1n");
           `}
         </Script>
         <ThemeProvider>
